51ab01a9aac877e02fa4abdf0b7fd8eb9f11a605
HaxeFloppa <123578256+HaxeFloppa@users.noreply.github.com>
2023-06-07 16:45:00 +0100

    Update boot.s

--- src/boot/boot.s
+++ src/boot/boot.s
@@ -1,4 +1,43 @@
+[org 0x7c00]
 bits 16
+mov ah, 00h
+mov al, 13h
+int 0x10
+mov ah, 0Ch
+mov al, 15
+mov bh, 1
+.loop:
+    inc cx
+    cmp cx, 70
+    je .nl
+    jmp .write_pix
+.nl:
+    inc dx
+    mov cx, 0
+    cmp dx, 30
+    je .end
+    jmp .write_pix
+.write_pix:
+    int 0x10
+    jmp .loop
+.end:
+mov ah, 0Eh
+mov al, '$'
+mov bl, 2
+int 0x10
+mov al, ' '
+int 0x10
+.input:
+    mov ah, 00h
+    int 0x16
+    mov ah, 0Eh
+    mov bl, 14
+    int 0x10
+    inc bx
+    cmp bx, 5
+    je .end2
+    jmp .input
+.end2:
 jmp $
 times 510-($-$$) db 0
 dw 0AA55h
